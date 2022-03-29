36da8b822a5b395e5941fb57f630ce71b00d9e5e
Lima <muslima.abdulla2002@gmail.com>
2022-03-29 19:44:49 +0600

    using functions

---
+++ js/scriptFunction.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// function showFirstMessage(text) {
+//    console.log(text);
+// }
+// showFirstMessage('Function is working');
+// const calc = (a, b) => {console.log(a+b)};
+
+// const str = 'test';
+// console.log(str.toUpperCase());
+
+// console.log(str.indexOf('s'));
+
+// const logg = "Hello Muslima";
+// console.log(logg.slice(6, 13));
+// console.log(logg.toUpperCase().substring(6, 13));
+// console.log(logg.substr(6, 7));
+
+const num = 12.5;
+console.log(Math.round(num));
+
+const test = "12.5px";
+console.log(parseFloat(test));
